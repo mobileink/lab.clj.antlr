f9006f2d50e079fde463ed949a331f45a1e9e5ef
Gregg Reynolds <dev@mobileink.com>
2014-05-20 11:03:03 -0500

    fix grun.sh for test rigging

--- grun.sh
+++ grun.sh
@@ -10,6 +10,6 @@
 
 ANTLR=$MAVEN_REPO/org/antlr
 
-java -cp ".:target/classes:$ANTLR/antlr4-runtime/4.2.2/antlr4-runtime-4.2.2.jar:$ANTLR/antlr4/4.2.2/antlr4-4.2.2.jar:$ANTLR/ST4/4.0.8/ST4-4.0.8.jar:$ANTLR/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar" \
+java -cp ".:target/classes:$ANTLR/antlr4-runtime/4.2.2/antlr4-runtime-4.2.2.jar:$ANTLR/antlr4/4.2.2/antlr4-4.2.2.jar:$ANTLR/ST4/4.0.8/ST4-4.0.8.jar:$ANTLR/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:$MAVEN_REPO/org/abego/treelayout/org.abego.treelayout.core/1.0.1/org.abego.treelayout.core-1.0.1.jar" \
     org.antlr.v4.runtime.misc.TestRig \
     $*;
