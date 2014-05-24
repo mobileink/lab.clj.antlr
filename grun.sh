4ae0c96fb0cbda59d10d4e82ab08931e06895273
Gregg Reynolds <dev@mobileink.com>
2014-05-24 07:44:51 -0500

    grun.sh error checking

--- grun.sh
+++ grun.sh
@@ -5,11 +5,11 @@
 
 usage(){
     echo "usage: $ ./grun.sh <grammar> <start-rule> <output-type> [file]"
-    echo "    where <output-type> == tokens | tree | gui | ps <filename>"
+    echo "    where <output-type> == tokens (default) | tree | gui | ps <filename>"
     echo "    [file] == optional file to parse; omit it to enter expressions at command line"
     echo "To see all options run without args:  $ ./grun.sh"
     echo "Example:"
-    echo "$ ./grun.sh Clojure file -tree"
+    echo "$ ./grun.sh foo start tokens"
     echo "(+ 1 2)"
     echo "<ctrl-D>"
     exit 1
@@ -20,15 +20,46 @@ usage(){
 [[ $# -eq 0 ]] && usage
 
 ANTLR=$MAVEN_REPO/org/antlr
+
 FILE=
+OUTFMT=-tokens
+
+if [ -n "$3" ]
+then
+    case $3 in
+	tokens)
+	    OUTFMT=-$3
+	    ;;
+	tree)
+	    OUTFMT=-$3
+	    ;;
+	gui)
+	    OUTFMT=-$3
+	    ;;
+	ps)
+	    OUTFMT=-$3
+	    ;;
+	*)
+	    if [ -f "test/grammar/$3" ]
+	    then
+		FILE=test/grammar/$3
+	    else
+		echo "Invalid output format $3; must be one of: tokens, tree, gui, ps <filename>"
+		exit 1
+	    fi
+    esac
+fi
+
 if [ -f "test/grammar/$4" ]
 then
     FILE=test/grammar/$4
 fi
 
-# set -x
+set -x
 
 java -cp ".:target/classes:$ANTLR/antlr4-runtime/4.2.2/antlr4-runtime-4.2.2.jar:$ANTLR/antlr4/4.2.2/antlr4-4.2.2.jar:$ANTLR/ST4/4.0.8/ST4-4.0.8.jar:$ANTLR/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:$MAVEN_REPO/org/abego/treelayout/org.abego.treelayout.core/1.0.1/org.abego.treelayout.core-1.0.1.jar" \
     org.antlr.v4.runtime.misc.TestRig \
     org.mobileink.antlr.$1 \
-    $2 -$3 $FILE
+    $2 \
+    $OUTFMT \
+    $FILE;
