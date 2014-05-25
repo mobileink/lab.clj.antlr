3d9d9cc5faefb47b7a1d06d7ccc8f378c6353495
Gregg Reynolds <dev@mobileink.com>
2014-05-25 08:37:27 -0500

    proj: use clj 1.6.0, criterium; grun.sh tweak

--- grun.sh
+++ grun.sh
@@ -55,7 +55,7 @@ then
     FILE=test/grammar/$4
 fi
 
-set -x
+# set -x
 
 java -cp ".:target/classes:$ANTLR/antlr4-runtime/4.2.2/antlr4-runtime-4.2.2.jar:$ANTLR/antlr4/4.2.2/antlr4-4.2.2.jar:$ANTLR/ST4/4.0.8/ST4-4.0.8.jar:$ANTLR/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:$MAVEN_REPO/org/abego/treelayout/org.abego.treelayout.core/1.0.1/org.abego.treelayout.core-1.0.1.jar" \
     org.antlr.v4.runtime.misc.TestRig \
--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src/java"]
   :target-path "target/%s"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.antlr/antlr4 "4.2.2"]
                  [org.antlr/antlr4-annotations "4.2.2"]
                  [org.antlr/antlr4-runtime "4.2.2"]
                  [org.antlr/ST4 "4.0.8"]
+                 [criterium "0.4.3"]
                  ])
