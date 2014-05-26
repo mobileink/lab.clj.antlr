efa939e7fc234eafab6cf6ec3b014de278cff211
Gregg Reynolds <dev@mobileink.com>
2014-05-26 03:46:24 -0500

    proj: don't compile on test; javac options; tools.namespace

--- project.clj
+++ project.clj
@@ -1,14 +1,24 @@
-(defproject org.mobileink.clajistics.antlr "0.1.0-SNAPSHOT"
+(defproject org.mobileink.antlr "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :target-path "target/%s"
+  :javac-options ["-target" "1.7" "-source" "1.6" "-Xlint:-options"]
+  ;; :target-path "target/%s"
+  ;;:test-paths ["test/grammar"]
+  ;; :test-selectors {:default (fn [m] (not (or (:integration m) (:regression m))))
+  ;;                  :integration :integration
+  ;;                  :regression :regression}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.antlr/antlr4 "4.2.2"]
                  [org.antlr/antlr4-annotations "4.2.2"]
                  [org.antlr/antlr4-runtime "4.2.2"]
                  [org.antlr/ST4 "4.0.8"]
                  [criterium "0.4.3"]
-                 ])
+                 ]
+  :prep-tasks [] ;; do not recompile on test
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
+  )
+
