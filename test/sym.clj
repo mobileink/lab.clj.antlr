ccc43df6f142c9ae0f7cd72c163bf7491148212c
Gregg Reynolds <dev@mobileink.com>
2014-05-26 04:56:39 -0500

    add test driver files

--- test/sym.clj
+++ test/sym.clj
@@ -1,4 +1,4 @@
-(ns sym-test
+(ns sym
   (:import [org.antlr.v4.runtime DefaultErrorStrategy]
            [symLexer])
   (:require [clojure.test :refer :all]
@@ -37,7 +37,11 @@
 
 (use-fixtures :once test-setup)
 
-(deftest sym01
-  (testing "minimal symbol"
-    (lex/lex-string "a//b(c" "sym")))
-    ;; (is (= 0 1))))
+;; (deftest sym01
+;;   (testing "minimal symbol"
+;;     (lex/lex-string "abc def" "sym")))
+
+(deftest symfile-01
+  (testing "minimal symbol file"
+    (lex/lex-file "sym" "test/data/sym/sym902.clj")))
+
