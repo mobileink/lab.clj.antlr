8d4a149e25eb2541eb5e298ffd5b4212fd6d8d88
Gregg Reynolds <dev@mobileink.com>
2014-05-27 16:56:46 -0500

    cleanup lex.clj

--- src/clojure/org/mobileink/antlr/lex.clj
+++ src/clojure/org/mobileink/antlr/lex.clj
@@ -1,6 +1,9 @@
 (ns org.mobileink.antlr.lex
-  (:require [criterium.core :as crit])
-  (:import [org.antlr.v4.runtime
+  (:require [criterium.core :as crit]
+            [byte-streams :as bs])
+  (:import [java.nio.ByteBuffer]
+           [java.net URL URLClassLoader]
+           [org.antlr.v4.runtime
             ANTLRInputStream
             ANTLRFileStream
             CharStream
@@ -13,36 +16,28 @@
             TokenStream]
            [org.antlr.v4.runtime.atn PredictionMode]
            [org.antlr.v4.runtime.tree ParseTree ParseTreeWalker]
-           [org.antlr.v4.runtime.misc TestRig]))
+           [org.antlr.v4.runtime.misc TestRig]
+           [clojure.lang DynamicClassLoader]))
 
-;; ANTLR Lexer Javadoc: http://www.antlr.org/api/Java-master/org/antlr/v4/runtime/Lexer.html
+;; ANTLR Lexer Javadoc:
+;; http://www.antlr.org/api/Java-master/org/antlr/v4/runtime/Lexer.html
 
 (defn lex-file
   [^String grammar ;; e.g. "org.mobileink.antlr.sym"
    ^String file ;; file to parse
-;;   rule ;; grammar rule symbol
    ]
   (let [
         lexname (str grammar "Lexer")
         infile (ANTLRFileStream. file)
         lxr (eval `(new ~(Class/forName lexname) nil))
-        x (.setInputStream lxr infile)
-        ;; we can get tokens directly from lexer:
-        ;; tokens (do (.reset lxr) (.getAllTokens lxr))
-        ;; but they don't have token index
-        ;; token streams keep track of token indices:
-        tokens (CommonTokenStream. lxr)
-        ;; ;; parsername (str grammar "Parser")
-        ;; ;; parsens (import-by-name parsername)
-        ;; parser (do
-        ;;          (println "getting parser")
-        ;;          (ClojureParser. tokens))
+        devnull (.setInputStream lxr infile)
+        ;; tokens (.getAllTokens lxr)) ;; list of all toks except EOF
+        tokstream (CommonTokenStream. lxr) ;; indexed list of tokens
         ]
     (do
-      (.fill tokens)
-;      (println "tokens: " (class tokens))
+      (.fill tokstream)
       (let
-          [seqtoks (iterator-seq (.iterator (.getTokens tokens)))]
+          [seqtoks (iterator-seq (.iterator (.getTokens tokstream)))]
         (do
           (doall (map (fn [t] (print (format "%s\n" (.toString t))))
                       seqtoks))
@@ -51,23 +46,13 @@
 
 (defn lex-string
   [^String grammar ;; e.g. "org.mobileink.antlr.sym"
-   ^String the-string ;; file to parse
-   ;; rule ;; grammar rule symbol
+   ^String the-string ;; file to scan
    ]
-
-  ;; TODO:  proxy <grammar>Lexer
-
-  (let [
-        lexname (str grammar "Lexer")
+  (let [lexname (str grammar "Lexer")
         strinput (ANTLRInputStream. the-string)
         lxr (eval `(new ~(Class/forName lexname) nil))
-        foo (.setInputStream lxr strinput)
-        ;; tokens (do (.reset lxr) (.getAllTokens lxr))
-        tokens (CommonTokenStream. lxr)
-        ;; parsername (str grammar "Parser")
-        ;; parsens (import-by-name parsername)
-        ;;lex (.setInputStream (symbol (new (str grammar "Lexer"))) input)
-        ]
+        devnull (.setInputStream lxr strinput)
+        tokens (CommonTokenStream. lxr)]
     (do
       (.fill tokens)
       (let
@@ -77,3 +62,4 @@
                       seqtoks))
           ))))
   nil)
+
